c8f77d12282ecca5a4f9f0826f8faa0646f21440
Aidara Moussa <msaidara@gmail.com>
2018-01-04 17:21:09 +0100

    Update Animer.java

--- zork/Animer.java
+++ zork/Animer.java
@@ -1,65 +1,14 @@
-import java.util.StringTokenizer;
+import java.util.Random;
 
-/**
- * Write a description of class Animer here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+public class Animer extends Data {
+    private final static String direction [] = {"nord", "sud", "est", "ouest"};
+    public Animer(String nom, int poids, boolean etat) {
+        this.nom = nom;
+        this.poids = poids;
+        this.etat = etat;
+    }
 
-public class Animer
-{
-    // instance variables - replace the example below with your own
-    private String nom;
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y    a sample parameter for a method
-     * @return        the sum of x and y 
-     */
-    public Animer(String nom){
-     this.nom=nom;
+    public Commande deplacerAleatoire(){
+        return new Commande("Aller",direction[new Random().nextInt(direction.length)]);
     }
-    
-    public String getNom()
-    {
-      return this.nom;
-    }
-    public Commande deplacerAleatoire(Piece pc)
-    {  
-        String[] d=new String[5];
-        String direction=pc.descriptionSorties();
-        StringTokenizer tok = new StringTokenizer(direction);
-        
-      int i=0,cpt=0;
-		while(tok.hasMoreTokens()) 
-		{
-		     d[i]=tok.nextToken();
-		    i++;
-		}	
-		for(i=1;i<5;i++)
-		  if(!(d[i].equals("")))
-		    cpt++;
-		    
-		if(cpt==0)
-		{ 
-		   return new Commande("retour","");
-		}
-		else
-		{
-		    
-           return new Commande("aller","nord");
-        }
-    }
-    public void deplacer(Piece pc,Piece ps)
-    {  
-       
-       if(pc.RechercheSupprime(this.getNom()))
-       {
-            ps.ajouAnimer(this);
-       }
-    }
-   
-    
 }
